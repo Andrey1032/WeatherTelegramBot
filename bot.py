de722f078a405ed35ea49f333d99533d6bb41158
Andrey1032 <@Andrey1032(Telegram)>
2025-10-21 16:22:17 +0500

    uptime

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ def schedule_loop():
 
 def subtract_hours(time_str):
     # Преобразуем строку типа '10:02' в объект datetime
-    time_obj = datetime.strptime(time_str, '%H:%M')
+    time_obj = datetime.strptime(time_str, '%H:%M:%S')
     
     # Отнимаем 5 часов
     new_time = time_obj - timedelta(hours=5)
